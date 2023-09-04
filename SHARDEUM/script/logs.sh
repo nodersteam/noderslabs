9f8e452dbfb7e38f9bd3d875ec5fe456b532a440
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2023-09-04 21:04:00 +0700

    Update logs.sh

--- SHARDEUM/script/logs.sh
+++ SHARDEUM/script/logs.sh
@@ -1,1 +1,15 @@
+#!/usr/bin/env bash
 
+docker-safe() {
+  if ! command -v docker &>/dev/null; then
+    echo "docker is not installed on this machine"
+    exit 1
+  fi
+
+  if ! docker "$@"; then
+    echo "Trying again with sudo..."
+    sudo docker "$@"
+  fi
+}
+
+(docker-safe logs -f shardeum-dashboard)
