45a9ac3708b1c5622b2a8f281556edf7bc573e2b
[NODERS]TEAM <94483941+nodersteam@users.noreply.github.com>
2023-10-17 21:18:51 +0700

    Update reward_information.py

--- SUI/suireward/reward_information.py
+++ SUI/suireward/reward_information.py
@@ -1,38 +1,52 @@
 import re
 import subprocess
+from utils import format_number
 
-def check_and_write_to_file(object_id):
+def get_reward_information():
     try:
-
         with open("reward.txt", "r") as f:
             existing_rewards = f.readlines()
+            print("Existing Reward Information:")
+            print(''.join(existing_rewards))
             existing_object_ids = set(re.findall(r'Object ID: (\S+)', ''.join(existing_rewards)))
     except FileNotFoundError:
         print("reward.txt file not found. Creating a new file.")
         existing_rewards = []
         existing_object_ids = set()
 
-    try:
+    command_output = subprocess.check_output("sui client objects", shell=True, stderr=subprocess.DEVNULL, universal_newlines=True)
+
+    object_ids = re.findall(r'0x[0-9a-f]{64}', command_output)
+    print("Retrieving reward information...")
+
+    updated_rewards = []
+
+    for object_id in object_ids:
+        if object_id in existing_object_ids:
+            continue
+
         object_output = subprocess.check_output(f"sui client object {object_id}", shell=True, stderr=subprocess.DEVNULL, universal_newlines=True)
 
-        if 'staking_pool::StakedSui' in object_output and 'principal:' in object_output:
+        if 'principal:' in object_output:
+            id_line = re.search(r'id:\s*(\S+)', object_output)
             principal_line = re.search(r'principal:\s*(\d+)', object_output)
 
-            if principal_line:
+            if id_line and principal_line:
+                object_id = id_line.group(1)
                 principal = int(principal_line.group(1)) / 1000000000
 
-                if principal > 1.0:
-                    with open("reward.txt", "a") as f:
-                        f.write(f"Object ID: {object_id}\nAmount: {principal} SUI\n")
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing command for objectId {object_id}: {e}")
+                if principal >= 1.0:
+                    output_line = f"Object ID: {object_id}\nAmount: {format_number(principal)} SUI\n"
 
-try:
-    command_output = subprocess.check_output("sui client objects", shell=True, stderr=subprocess.DEVNULL, universal_newlines=True)
-    object_ids = re.findall(r'0x[0-9a-f]{64}', command_output)
-except subprocess.CalledProcessError as e:
-    print(f"Error executing command: {e}")
-    object_ids = []
+                    updated_rewards.append(output_line)
+                    existing_object_ids.add(object_id)
 
-for object_id in object_ids:
-    check_and_write_to_file(object_id)
+    if updated_rewards:
+        with open("reward.txt", "a") as f:
+            for reward in updated_rewards:
+                f.write(reward)
+        print("Reward information updated.")
+        print("Rewards found in the search:")
+        print(''.join(updated_rewards))
+    else:
+        print("No reward information found.")
